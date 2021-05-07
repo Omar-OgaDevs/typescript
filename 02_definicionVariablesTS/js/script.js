9245e80b91db96e5fac6edaf68d443adab2f123f
omar.ogadevs <omar.ogadevs@outlook.com>
2021-05-07 13:00:01 -0500

    definicion de variables en typescript

---
+++ 02_definicionVariablesTS/js/script.js
@@ -0,0 +1,15 @@
+"use strict";
+let saludo = "¡Hola Mundo desde TypeScript!";
+saludo = "Hola Evelyn";
+let numero;
+numero = 10;
+let cualquiera;
+cualquiera = "cadena";
+cualquiera = 10;
+const NUMERO_PI = 3.141516;
+function saludar() {
+    console.log(saludo);
+    console.log(numero);
+    console.log(NUMERO_PI);
+}
+saludar();
---
+++ 02_definicionVariablesTS/js/script.ts
@@ -0,0 +1,18 @@
+let saludo ="¡Hola Mundo desde TypeScript!";
+saludo="Hola Evelyn";
+
+let numero:number;
+numero=10;
+
+let cualquiera: any;
+cualquiera="cadena";
+cualquiera=10;
+
+const NUMERO_PI=3.141516;
+
+function saludar(){
+  console.log(saludo);
+  console.log(numero);
+  console.log(NUMERO_PI);
+}
+saludar();
