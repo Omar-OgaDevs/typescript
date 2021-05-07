5d4c6959e0000740ca0d701b0d1e9e3a928290c2
omar.ogadevs <omar.ogadevs@outlook.com>
2021-05-07 16:44:31 -0500

    generics en typescript

---
+++ 05_generics/js/script.js
@@ -0,0 +1,5 @@
+"use strict";
+let arregloNumeros;
+arregloNumeros = [7, 12, 13, 5, 25];
+console.log(arregloNumeros);
+console.log(arregloNumeros[2]);
---
+++ 05_generics/js/script.ts
@@ -0,0 +1,6 @@
+let arregloNumeros:Array<number>;
+
+arregloNumeros=[7,12,13,5,25];
+
+console.log(arregloNumeros);
+console.log(arregloNumeros[2]);
